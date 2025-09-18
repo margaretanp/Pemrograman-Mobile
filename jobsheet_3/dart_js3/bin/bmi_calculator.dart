68cf85e532d0903cf6a39b3feedd677da9918fd7
margareta <margaretanovia1911@gmail.com>
2025-09-18 19:36:37 +0700

    add main function

--- jobsheet_3/dart_js3/bin/bmi_calculator.dart
+++ jobsheet_3/dart_js3/bin/bmi_calculator.dart
@@ -1,3 +1,13 @@
+void main() {
+  List<Map<String, dynamic>> riwayat = []; // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+}
+
+// Fungsi untuk menghitung BMI dan menyimpan hasil ke riwayat
 void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   // Konversi tinggi dari cm ke m
   double tinggiM = tinggiCm / 100;
