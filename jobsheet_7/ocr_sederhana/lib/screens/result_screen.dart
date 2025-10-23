630accbda647a7055525c039e7323042a1f09bcb
margareta <margaretanovia1911@gmail.com>
2025-10-23 16:29:27 +0700

    UTS: Selesai Soal 3 - Implementasi Flutter TTS

--- jobsheet_7/ocr_sederhana/lib/screens/result_screen.dart
+++ jobsheet_7/ocr_sederhana/lib/screens/result_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:ocr_sederhana/screens/home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
 
   const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();
+    _initTts();
+  }
+
+  Future<void> _initTts() async {
+    await flutterTts.setLanguage("id-ID");
+    await flutterTts.setSpeechRate(0.5);
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+    super.dispose();
+  }
+
+  Future<void> _speak() async {
+    await flutterTts.speak(widget.ocrText);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +42,32 @@ class ResultScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Hasil OCR')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Text(ocrText, style: const TextStyle(fontSize: 16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  widget.ocrText,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: _speak,
+                icon: const Icon(Icons.volume_up),
+                label: const Text('Bacakan Teks'),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushAndRemoveUntil(
-            context,
-            MaterialPageRoute(builder: (context) => const HomeScreen()),
-            (route) => false,
-          );
-        },
-        child: const Icon(Icons.home),
+        onPressed: _speak,
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.volume_up),
       ),
     );
   }
