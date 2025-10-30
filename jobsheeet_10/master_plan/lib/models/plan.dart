8648891a96a04fcc460139b4ae82d39979dec34d
margareta <margaretanovia1911@gmail.com>
2025-10-30 21:09:50 +0700

    feat: Implement PlanProvider and enhance Plan functionality with ValueNotifier

--- jobsheeet_10/master_plan/lib/models/plan.dart
+++ jobsheeet_10/master_plan/lib/models/plan.dart
@@ -1,8 +1,11 @@
 import './task.dart';
 
 class Plan {
+  int get completedCount => tasks.where((task) => task.complete).length;
+  String get completenessMessage =>
+      '$completedCount out of ${tasks.length} tasks';
   final String name;
   final List<Task> tasks;
-  
+
   const Plan({this.name = '', this.tasks = const []});
 }
